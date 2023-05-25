9c0757f17db0749296b49379fc52f90d734b73d7
hanieh <haniehrahim78@gmail.com>
2023-05-25 10:21:21 -0700

    my services

--- script.js
+++ script.js
@@ -17,7 +17,45 @@ closeMenu.addEventListener('click' , function(){
     menuRight.style.opacity = 0
     menuRight.style.visibility = 'hidden'
 })
+// for sliders =========================================================================================
+var arrR = document.querySelector('.arrR')
+var arrL = document.querySelector('.arrL')
+var boxSlider = document.querySelectorAll('.box-slider')
 
+var x = 0
+var y = 0
+arrR.addEventListener('click' , function(){
+    if ( x == 0 ) {
+        x = 1
+        y += 300
+        boxSlider.forEach(item => {
+            item.style.transform = `translateX(${y}px)`
+        });
+    } else if ( x == 1){
+        x = 0
+        y += 300
+        boxSlider.forEach(item => {
+            item.style.transform = `translateX(${y}px)`
+        });
+    }
+})
+
+
+arrL.addEventListener('click' , function(){
+    if ( x == 0 ) {
+        x = 1
+        y -= 300
+        boxSlider.forEach(item => {
+            item.style.transform = `translateX(${y}px)`
+        });
+    } else if ( x == 1){
+        x = 0
+        y -= 300
+        boxSlider.forEach(item => {
+            item.style.transform = `translateX(${y}px)`
+        });
+    }
+})
 
 
 
